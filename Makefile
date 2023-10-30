36ae1133ad84d2643eeab7679f183e4423a8fd75
Sang-Woo Jun <swjun@ics.uci.edu>
2023-10-30 22:09:32 +0000

    fixed some Makefile issues

--- Makefile
+++ Makefile
@@ -212,3 +212,4 @@ cleanall: clean
 	-$(RMDIR) package.*
 	-$(RMDIR) _x* *xclbin.run_summary qemu-memory-_* emulation _vimage pl* start_simulation.sh *.xclbin
 	-$(RMDIR) ./tmp_kernel_pack* ./packaged_kernel* 
+	-$(RMDIR) $(KERNELDIR)/obj/
--- config.mk
+++ config.mk
@@ -6,6 +6,6 @@ bsv_kernel: $(KERNELDIR)/*.bsv $(KERNELDIR)/*.v
 	cd $(KERNELDIR) && $(MAKE)
 
 
-$(TEMP_DIR)/kernel.xo: scripts/package_kernel.tcl scripts/gen_xo.tcl src/kernel/obj/verilog/*.v bsv_kernel
+$(TEMP_DIR)/kernel.xo: scripts/package_kernel.tcl scripts/gen_xo.tcl bsv_kernel
 	mkdir -p $(TEMP_DIR)
 	$(VIVADO) -mode batch -source scripts/gen_xo.tcl -tclargs $(TEMP_DIR)/kernel.xo mkKernel $(TARGET) $(PLATFORM) $(XSA)
